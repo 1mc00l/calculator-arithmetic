ac58d885b4d7f84002aa206315ee771480343ff3
cool <you@example.com>
2022-08-26 19:30:57 +0200

    start

--- calculator.c
+++ calculator.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	printf("hello world");
+	char operator;	
+	int num1;
+	int num2;
+	printf("(+, -, /)\n\n");
+	printf("operator?");
+	scanf("%c", &operator);
+	printf("%c", operator);	
 	return 0;
 }
